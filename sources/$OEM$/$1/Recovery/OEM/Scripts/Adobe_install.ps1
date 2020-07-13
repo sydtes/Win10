0712fddb4706af459358facf6433f33f43041474
sydtes <53543809+sydtes@users.noreply.github.com>
2020-07-13 12:41:44 +1000

    Create Adobe_install.ps1

---
+++ sources/$OEM$/$1/Recovery/OEM/Scripts/Adobe_install.ps1
@@ -0,0 +1,7 @@
+$Folder = "C:\Recovery\OEM\Software\Adobe\"
+$File = Get-ChildItem -Path "$Folder*" -Include *.exe
+$Path = $Folder + $file.Name
+
+# Install quietly
+"Installing.. Adobe"
+Start-Process $Path /sPB -NoNewWindow -Wait
